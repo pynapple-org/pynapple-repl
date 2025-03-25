1f2ddc2030f130c596060e66f8d87aadd74665e1
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-03-25 17:58:08 -0400

    update

--- install_nap.py
+++ install_nap.py
@@ -1,3 +1,8 @@
 import micropip
+import asyncio
 
-await micropip.install("https://pynapple-org.github.io/pynapple-repl/files/pynapple-0.8.5-py3-none-any.whl")
+async def install_nap():
+    await micropip.install("https://pynapple-org.github.io/pynapple-repl/files/pynapple-0.8.5-py3-none-any.whl")
+
+asyncio.run(install_nap())
+
